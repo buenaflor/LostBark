5efd44231f2f2b48eb0332ae1149f497bf489c4d
giancarlo_buenaflor@yahoo.com <giancarlo_buenaflor@yahoo.com>
2022-03-15 03:49:13 +0100

    testing because of heroku..

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class MyClient(discord.Client):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        print("Received message: " + message.content)
+        print("Received message: " + message)
         if message.content.startswith(Commands.MatsGoldPrice.value):
             print("Inside mats gold")
             if Options.TotalPriceOfProduct.value in message.content and Options.AmountReceived.value in message.content and Options.ExchangeRate.value in message.content:
