33098b6be1dbce754d9cfa190f25e7c1cb2a7978
giancarlo_buenaflor@yahoo.com <giancarlo_buenaflor@yahoo.com>
2022-03-17 15:26:05 +0100

    add weekly, daily and commands

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from utils import remove_nonnumeric_chars, get_first_occurrence
 class Commands(enum.Enum):
     MatsGoldPrice = "!mtg"
     Daily = "!daily"
+    Weekly = "!weekly"
+    AllCommands = "!commands"
 
     # Returns a text of usage in case of false input
     def usage(self):
@@ -69,11 +71,29 @@ class MyClient(discord.Client):
                 await message.channel.send(Commands.MatsGoldPrice.usage())
 
         if message.content.startswith(Commands.Daily.value):
-            # todo res = ""
-            await message.channel.send(res)
+            res = "2x Chaos Dungeons\n"
+            res += "2x Guardian Raids\n"
+            res += "3x Una's Tasks\n"
+            res += "Anguished Isle\n"
+            res += "Chaos Gate / Field Boss / Adventure Island\n"
+            res += "Guild Task / Guild Donation / Guild Research Support\n"
+            await message.channel.send(italic(res))
 
+        if message.content.startswith(Commands.Weekly.value):
+            res = "Abyss Dungeon\n"
+            res += "3x Weekly Una's Tasks\n"
+            res += "Buy Guild Shop Products\n"
+            res += "Buy Mats on Pirate Ship\n"
+            res += "Buy Mats on Dungeon Exchange Shop\n"
+            res += "Buy Mats on Grand Prix\n"
+            await message.channel.send(italic(res))
+
+        if message.content.startswith(Commands.AllCommands.value):
+            res = bold("!mtg") + " -> Mari's Shop products to gold\n"
+            res += bold("!daily") + " -> List of daily tasks\n"
+            res += bold("!weekly") + " -> List of weekly tasks\n"
+            await message.channel.send(italic(res))
 
 client = MyClient()
 
-# client.run(os.environ.get('token'))
-client.run("OTUzMDU1MDIwMTA5MTY0NzE0.Yi-_pw.90cyNzEUbsL4utI8e6BbDzM7ERs")
+client.run(os.environ.get('token'))
